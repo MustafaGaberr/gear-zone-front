a5ac33985fe2c2e42c577a12053b29ebf6aa1736
Mustafa Gaber <DrMustafaGaber@gmail.com>
2026-01-29 05:13:52 +0200

    Revert "cart page api calls,"
    This reverts commit 37087d619d896b6bd4e5b48cf33f78e5a3f580a8.

--- src/app/core/interceptors/header.ts
+++ src/app/core/interceptors/header.ts
@@ -1,20 +1,23 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { Token } from '@angular/compiler';
 import { inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 export const headersInterceptor: HttpInterceptorFn = (req, next) => {
-  const cookieService = inject(CookieService);
+  
+  const cookieService=inject(CookieService)
 
-  if (cookieService.check('token')) {
-    const token = cookieService.get('token');
-
-    if (token && token.trim()) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
+  if(cookieService.check('token')){
+   
+     req=req.clone({
+    //   setHeaders:{
+    //     token:cookieService.get('token')
+    //   },
+     setHeaders: {
+      Authorization: `Bearer ${cookieService.get('token')}`
     }
+    })
+   
   }
 
   return next(req);
